6ee30eedbdf2d45fa9d522bfd62c0f6bc9b85b82
acceedo <naveen@acceedo.in>
2025-05-29 12:40:08 +0530

    meterial input page work rework

--- src/app/modules/materialinput/casting-input/casting-input.component.ts
+++ src/app/modules/materialinput/casting-input/casting-input.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { loadCastingInputs } from '../store/casting.actions';
-import { CastingInput } from '../../../models/casting-input.model';
+import {
+  loadCastingInputs,
+  updateCastingInput,
+  loadCoreInputs,
+  updateCoreInput,
+  loadMouldingInputs,
+  updateMouldingInput
+} from '../store/casting.actions';
+import { CastingInput, CoreInput, MouldingInput } from '../../../models/casting-input.model';
 import { map } from 'rxjs/operators';
-import { updateCastingInput } from '../store/casting.actions';
 
 @Component({
   selector: 'app-casting-input',
@@ -12,7 +18,10 @@ import { updateCastingInput } from '../store/casting.actions';
 })
 export class CastingInputComponent implements OnInit {
   castingData: CastingInput[] = [];
-  displayedColumns: string[] = [
+  coreData: CoreInput[] = [];
+  mouldingData: MouldingInput[] = [];
+
+  displayedCastingColumns: string[] = [
     '_id',
     'CastingWeight',
     'Cavities',
@@ -21,21 +30,52 @@ export class CastingInputComponent implements OnInit {
     'Yeild',
     'MaterialReturned'
   ];
-  editMode: { [key: string]: { [field: string]: boolean } } = {}; // Track edit mode for each field of each item
-  editableData: { [key: string]: CastingInput } = {};
+  displayedCoreColumns: string[] = [
+    '_id',
+    'CoreSand',
+    'CoreWeight',
+    'ShootingPerShift',
+    'coresPerMould'
+  ];
+  displayedMouldingColumns: string[] = [
+    '_id',
+    'BakeMoulding',
+    'MouldingWeight',
+    'MouldsPerHeat'
+  ];
 
+  editMode: {
+    casting: { [key: string]: { [field: string]: boolean } };
+    core: { [key: string]: { [field: string]: boolean } };
+    moulding: { [key: string]: { [field: string]: boolean } };
+  } = { casting: {}, core: {}, moulding: {} };
 
+  editableData: {
+    casting: { [key: string]: CastingInput };
+    core: { [key: string]: CoreInput };
+    moulding: { [key: string]: MouldingInput };
+  } = { casting: {}, core: {}, moulding: {} };
 
-
-  constructor(private store: Store<{ casting: { data: CastingInput[] } }>) {}
+  constructor(private store: Store<{
+    casting: {
+      data: CastingInput[];
+      coreData: CoreInput[];
+      mouldingData: MouldingInput[];
+    }
+  }>) {}
 
   ngOnInit(): void {
+    // Dispatch actions to load all data
     this.store.dispatch(loadCastingInputs());
+    this.store.dispatch(loadCoreInputs());
+    this.store.dispatch(loadMouldingInputs());
 
+    // Subscribe to casting data
     this.store.pipe(select('casting'), map(state => state.data)).subscribe(data => {
       this.castingData = data;
-      this.castingData.forEach(item => {
-        this.editMode[item._id] = {
+      data.forEach(item => {
+        this.editableData.casting[item._id] = { ...item };
+        this.editMode.casting[item._id] = {
           CastingWeight: false,
           Cavities: false,
           PouringWeight: false,
@@ -47,57 +87,103 @@ export class CastingInputComponent implements OnInit {
       console.log('Casting Inputs:', data);
     });
 
-    this.store.pipe(select('casting'), map(state => state.data)).subscribe(data => {
-  this.castingData = data;
+    // Subscribe to core data
+    this.store.pipe(select('casting'), map(state => state.coreData)).subscribe(data => {
+      this.coreData = data;
+      data.forEach(item => {
+        this.editableData.core[item._id] = { ...item };
+        this.editMode.core[item._id] = {
+          CoreSand: false,
+          CoreWeight: false,
+          ShootingPerShift: false,
+          coresPerMould: false
+        };
+      });
+      console.log('Core Inputs:', data);
+    });
 
-  data.forEach(item => {
-    this.editableData[item._id] = { ...item };
-    this.editMode[item._id] = {
-      CastingWeight: false,
-      Cavities: false,
-      PouringWeight: false,
-      CastingWeightPerKg: false,
-      Yeild: false,
-      MaterialReturned: false
-    };
-  });
-});
-
+    // Subscribe to moulding data
+    this.store.pipe(select('casting'), map(state => state.mouldingData)).subscribe(data => {
+      this.mouldingData = data;
+      data.forEach(item => {
+        this.editableData.moulding[item._id] = { ...item };
+        this.editMode.moulding[item._id] = {
+          BakeMoulding: false,
+          MouldingWeight: false,
+          MouldsPerHeat: false
+        };
+      });
+      console.log('Moulding Inputs:', data);
+    });
   }
 
-  toggleEditMode(id: string, field: string): void {
-    this.editMode[id][field] = !this.editMode[id][field];
+  // Toggle edit mode for casting, core, or moulding
+  toggleEditMode(type: 'casting' | 'core' | 'moulding', id: string, field: string): void {
+    this.editMode[type][id][field] = !this.editMode[type][id][field];
   }
 
-saveField(item: CastingInput, field: string): void {
-  const id = item._id;
-  const updatedValue = this.editableData[id][field];
+  // Save field for casting, core, or moulding
+  saveField(type: 'casting' | 'core' | 'moulding', item: CastingInput | CoreInput | MouldingInput, field: string): void {
+    const id = item._id;
+    const updatedValue = this.editableData[type][id][field];
 
-  // Update the specific field in editableData
-  this.editableData[id][field] = updatedValue;
+    this.editableData[type][id][field] = updatedValue;
 
-  // Build the full updated object including _id
-  const updatedItem: CastingInput = {
-    _id: id,
-    CastingWeight: this.editableData[id].CastingWeight,
-    Cavities: this.editableData[id].Cavities,
-    PouringWeight: this.editableData[id].PouringWeight,
-    CastingWeightPerKg: this.editableData[id].CastingWeightPerKg,
-    Yeild: this.editableData[id].Yeild,
-    MaterialReturned: this.editableData[id].MaterialReturned
-  };
+    if (type === 'casting') {
+      const updatedItem: CastingInput = {
+        _id: id,
+        CastingWeight: this.editableData.casting[id].CastingWeight,
+        Cavities: this.editableData.casting[id].Cavities,
+        PouringWeight: this.editableData.casting[id].PouringWeight,
+        CastingWeightPerKg: this.editableData.casting[id].CastingWeightPerKg,
+        Yeild: this.editableData.casting[id].Yeild,
+        MaterialReturned: this.editableData.casting[id].MaterialReturned
+      };
+      console.log(`Dispatching update for casting item ${id}:`, updatedItem);
+      this.store.dispatch(updateCastingInput({ id, data: updatedItem }));
+    } else if (type === 'core') {
+      const updatedItem: CoreInput = {
+        _id: id,
+        CoreSand: this.editableData.core[id].CoreSand,
+        CoreWeight: this.editableData.core[id].CoreWeight,
+        ShootingPerShift: this.editableData.core[id].ShootingPerShift,
+        coresPerMould: this.editableData.core[id].coresPerMould,
+        Cavities: this.editableData.core[id].Cavities
+      };
+      console.log(`Dispatching update for core item ${id}:`, updatedItem);
+      this.store.dispatch(updateCoreInput({ id, data: updatedItem }));
+    } else if (type === 'moulding') {
+      const updatedItem: MouldingInput = {
+        _id: id,
+        BakeMoulding: this.editableData.moulding[id].BakeMoulding,
+        MouldingWeight: this.editableData.moulding[id].MouldingWeight,
+        MouldsPerHeat: this.editableData.moulding[id].MouldsPerHeat
+      };
+      console.log(`Dispatching update for moulding item ${id}:`, updatedItem);
+      this.store.dispatch(updateMouldingInput({ id, data: updatedItem }));
+    }
 
-  console.log(`Dispatching update for item ${id}:`, updatedItem);
+    this.editMode[type][id][field] = false;
+  }
 
-  // Dispatch the update with full CastingInput object
-  this.store.dispatch(updateCastingInput({ id, data: updatedItem }));
-
-  this.editMode[id][field] = false;
-}
-
-
-
-  cancelEdit(id: string, field: string): void {
-    this.editMode[id][field] = false;
+  // Cancel edit for casting, core, or moulding
+  cancelEdit(type: 'casting' | 'core' | 'moulding', id: string, field: string): void {
+    this.editMode[type][id][field] = false;
+    if (type === 'casting') {
+      const originalItem = this.castingData.find(item => item._id === id);
+      if (originalItem) {
+        this.editableData.casting[id] = { ...originalItem };
+      }
+    } else if (type === 'core') {
+      const originalItem = this.coreData.find(item => item._id === id);
+      if (originalItem) {
+        this.editableData.core[id] = { ...originalItem };
+      }
+    } else if (type === 'moulding') {
+      const originalItem = this.mouldingData.find(item => item._id === id);
+      if (originalItem) {
+        this.editableData.moulding[id] = { ...originalItem };
+      }
+    }
   }
 }
